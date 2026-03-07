42f30ff837dcc0f2898f5567a0d2f6cd4ebab7fa
bjrajafju <bjrajafju@gmail.com>
2026-03-07 22:15:05 +0000

    criei o projeto

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import pool from "./config/db.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/db-test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({
+            status: "ok",
+            time: result.rows[0],
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "DB connection failed" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
